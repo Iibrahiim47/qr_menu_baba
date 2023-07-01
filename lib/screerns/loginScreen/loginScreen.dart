c1e640bec57638750b3cc82e2ac8cbe943e24061
iibrahiim47 <iibrahiim53@gmail.com>
2023-07-01 17:13:05 +0400

    modification

--- lib/screerns/loginScreen/loginScreen.dart
+++ lib/screerns/loginScreen/loginScreen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names, deprecated_member_use, prefer_typing_uninitialized_variables
 import 'package:flutter/material.dart';
 import 'package:qr_menu_baba/screerns/homescreen/home_screen.dart';
+import 'package:qr_menu_baba/screerns/review_form.dart/review.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -100,7 +101,14 @@ class _LoginScreenState extends State<LoginScreen> {
                         borderRadius: BorderRadius.circular(15),
                       ),
                       child: InkWell(
-                        onTap: () {},
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const ReviesPage(),
+                            ),
+                          );
+                        },
                         child: Container(
                           decoration: BoxDecoration(
                             color: const Color(0xffDB6439),
